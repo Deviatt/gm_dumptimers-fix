563de6bfcee108e54872c2e9ef9a7015e915a1e1
zohart75 <40930644+zohart75@users.noreply.github.com>
2022-12-25 17:37:27 +0500

    Update fix.cpp

--- fix.cpp
+++ fix.cpp
@@ -8,6 +8,7 @@ static SourceSDK::FactoryLoader icvar_loader("vstdlib", true, IS_SERVERSIDE, "bi
 GMOD_MODULE_OPEN() {
     ICvar *icvar = icvar_loader.GetInterface<ICvar>(CVAR_INTERFACE_VERSION);
     ConCommandBase *lua_dumptimers_sv = icvar->FindCommandBase("lua_dumptimers_sv");
+    
     if (lua_dumptimers_sv) icvar->UnregisterConCommand(lua_dumptimers_sv);
     return 0;
 }
