20ab0e86aa480a90d988591474fa0ddbd9c60f9c
zohart75 <40930644+zohart75@users.noreply.github.com>
2022-12-25 17:38:48 +0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -6,15 +6,13 @@ newoption {
     value = "path to garrysmod_common directory"
 }
 
-local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
-	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
-include(gmcommon)
+include(assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"), "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory"))
 
 CreateWorkspace {name = "dumptimers_fix"}
-    CreateProject {serverside = true}
-    IncludeLuaShared()
-    IncludeDetouring()
-    IncludeScanning()
-    IncludeSDKCommon()
-    IncludeSDKTier0()
-    files {"fix.cpp"}
+CreateProject {serverside = true}
+IncludeLuaShared()
+IncludeDetouring()
+IncludeScanning()
+IncludeSDKCommon()
+IncludeSDKTier0()
+files {"fix.cpp"}
