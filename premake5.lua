c8c38959c95e471e44aeb86c95777269f58c0e8e
Deviatt <70473864+Deviatt@users.noreply.github.com>
2022-12-26 13:56:10 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -9,10 +9,6 @@ newoption {
 include(assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"), "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory"))
 
 CreateWorkspace {name = "dumptimers_fix"}
-CreateProject {serverside = true}
-IncludeLuaShared()
-IncludeDetouring()
-IncludeScanning()
-IncludeSDKCommon()
-IncludeSDKTier0()
-files {"fix.cpp"}
+    CreateProject {serverside = true}
+        IncludeSDKCommon()
+        files {"fix.cpp"}
