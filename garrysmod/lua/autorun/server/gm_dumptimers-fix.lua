f3bd48c23ca60363e6381f6035ca696e8935b8e9
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2023-01-04 03:22:57 +0300

    anti spam

--- garrysmod/lua/autorun/server/gm_dumptimers-fix.lua
+++ garrysmod/lua/autorun/server/gm_dumptimers-fix.lua
@@ -6,9 +6,18 @@ local log = false -- log people that try to use this exploit?
 
 if (punish or log) == false then concommand.Remove("lua_dumptimers_sv") return end
 
+local lastLog = {}
+
+local function logAdd(ply, msg)
+    if (lastLog[ply] or 0) > CurTime() then return end
+    lastLog[ply] = CurTime() + 5 -- 1 log per 5 seconds (anti spam)
+    
+    file.Append("lua_dumptimers_sv.txt", msg)
+end
+
 concommand.Add("lua_dumptimers_sv", function(ply)
     if log then
-        file.Append("lua_dumptimers_sv.txt", string.format("%s (%s), %s\n", ply:Nick(), ply:SteamID64(), os.date("!%H:%M:%S, %d.%m.%Y")))
+        logAdd(ply, string.format("%s (%s), %s\n", ply:Nick(), ply:SteamID64(), os.date("!%H:%M:%S, %d.%m.%Y")))
     end
 
     if punish then
